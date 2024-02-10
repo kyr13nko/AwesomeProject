509daa6cd4255452f7bfe9ceec1e0f18e3432236
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2024-02-10 14:15:17 +0200

    init project

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+          <TextInput
+            value={name}
+            onChangeText={setName}
+            placeholder="Username"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password"
+            secureTextEntry
+            style={styles.input}
+          />
+          <Button title="Login" style={styles.input} onPress={onLogin} />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
   },
 });
+
+// import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+// export default function App() {
+//   const signIn = () => {
+//     console.debug("Welcome!");
+//   };
+//   return (
+//     <SafeAreaView style={{ flex: 1 }}>
+//       <View style={styles.container}>
+//         <View style={styles.titleContainer}>
+//           <Text style={styles.welcomeTitle}>Welcome to Olx Clone</Text>
+//         </View>
+//         <View style={styles.formContainer}>
+//           <Text style={styles.formTitle}>Sign In</Text>
+//           <View>
+//             <Text>Email</Text>
+//             <TextInput style={styles.input} placeholder="Input your email" autoComplete="email" />
+//           </View>
+//           <View>
+//             <Text>Password</Text>
+//             <TextInput
+//               style={styles.input}
+//               placeholder="Input your password"
+//               autoComplete="password"
+//             />
+//           </View>
+//           <TouchableOpacity style={styles.button} onPress={signIn}>
+//             <Text style={styles.buttonTitle}>Sign In</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {},
+//   titleContainer: {},
+//   welcomeTitle: {},
+//   formContainer: {},
+//   formTitle: {},
+//   input: {},
+//   button: {},
+//   buttonTitle: {},
+// });
